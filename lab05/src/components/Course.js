8c08c7e9527d8ddd5b4bb610b5bde79ecd41f04e
drandredev <drandredev@drandredev.localdomain>
2021-04-14 22:52:41 -0500

    Punto 2

--- lab05/src/components/Course.js
+++ lab05/src/components/Course.js
@@ -2,11 +2,16 @@ import Part from './Part'
 import Header from './Header'
 import Content from './Content'
 
+
+
 function Course(props) {
+
+    const total = props.course.parts.reduce((s, {exercises}) => s + exercises,0 )
     return (
         <div>
            <Header titulo={props.course.name}/>
            <Content partes={props.course.parts}/>
+           <p>Total de los Exercises: {total}</p>
         </div>
     )
 }
